c43d4d5d2f7889a70bab721cc036c20887009860
AnjaliMahawar <anjalimahawar011@gmail.com>
2022-01-10 21:46:12 +0530

    neww

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//import area 
+//import defultimport from "some_library_of _notemodule
+//import defultimport from "./some_library_of_relativ_file"
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyReactDOM from './MyClass';
+//ReactDOM.render(what,where);
+//ReactDOM.render();
+MyReactDOM.render("HAPPY","NEW YEAR");
+
